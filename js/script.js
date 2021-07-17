9140ab41a333438870577e525328cfda13e495cf
Ashton Walden <ashtonwalden@icloud.com>
2021-07-17 11:52:55 -0600

    tried adding type to script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-var name = prompt("Enter your name:");
+var user = prompt("Enter your name:");
 var age = prompt("Enter your age:");
 var month = prompt("Enter the month you were born:");
 
-alert("Happy Birthday " + name + "you are " + age + 
-"years old and you were born in " + month + "!");
+var ageNum = parseInt(age);
+
+if (ageNum < 50) {
+    alert("You are " + ageNum + ". Wow! You are still young!");
+}
+else if (ageNum == 50) {
+    alert("50 years old!")
+}
+else if (ageNum > 50) {
+    alert("You are " + ageNum + ". Dang, you are old!");
+}
+else {
+    alert("Please enter a number and try again.")
+}
+
+alert("Happy Birthday " + user + " you are " + age + 
+" years old and you were born in " + month + "!");
