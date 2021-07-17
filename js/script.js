2e2b66c2fa93eb9e1e4db8ab2f23af9bf3d222e2
Ashton Walden <ashtonwalden@icloud.com>
2021-07-17 11:18:27 -0600

    added js prompts and alert

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var name = prompt("Enter your name:");
+var age = prompt("Enter your age:");
+var month = prompt("Enter the month you were born:");
+
+alert("Happy Birthday " + name + "you are " + age + 
+"years old and you were born in " + month + "!");
